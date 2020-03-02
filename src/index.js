743943497ee1b977aed566c3028d3d526f8647b3
alexmalott <alex@alexmalott.com>
2020-03-02 11:04:15 -0700

    Added custom CSS support.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
+import './scss/custom.scss';
 import 'font-awesome/css/font-awesome.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
